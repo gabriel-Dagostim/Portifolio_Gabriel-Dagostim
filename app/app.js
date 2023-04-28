cdfd2cbacae421230bc1f387268364c9d9fe109a
Gabriel Henrique Dagostim Simões <tomuitoop@gmail.com>
2023-04-27 21:54:38 -0300

    new

--- app/app.js
+++ app/app.js
@@ -1,3 +1,21 @@
 //background-color: #3ab723;
 
 //#25f700
+
+var displayio = document.getElementById('minhaDiv').style.display;
+alert(displayio)
+
+function Mudarestado() {
+    displayio = document.getElementById("minhaDivpp").style.display;
+   alert(displayio)
+    if(displayio == "none"){
+        
+                document.getElementById("minhaDivpp").style.display = 'block';}
+
+                if(displayio == "block"){
+                    
+                            document.getElementById("minhaDivpp").style.display = 'none';}
+   
+}
+    
+
