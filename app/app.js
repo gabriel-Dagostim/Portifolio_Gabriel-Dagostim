7fb7dab300467ef9a2f18120493b10f0243171b4
Gabriel Henrique Dagostim Simões <tomuitoop@gmail.com>
2023-05-08 09:19:14 -0300

    botão funcional

--- app/app.js
+++ app/app.js
@@ -2,20 +2,7 @@
 
 //#25f700
 
-var displayio = document.getElementById('minhaDiv').style.display;
-alert(displayio)
 
-function Mudarestado() {
-    displayio = document.getElementById("minhaDivpp").style.display;
-   alert(displayio)
-    if(displayio == "none"){
-        
-                document.getElementById("minhaDivpp").style.display = 'block';}
 
-                if(displayio == "block"){
-                    
-                            document.getElementById("minhaDivpp").style.display = 'none';}
-   
-}
     
 
