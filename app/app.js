bba2030ef8ff20a9b17e9c2ae485cd012c57cb4f
Gabriel Henrique Dagostim Simões <tomuitoop@gmail.com>
2023-05-11 03:28:22 -0300

    Responsividade e Habilidades

--- app/app.js
+++ app/app.js
@@ -2,15 +2,16 @@
 
 //#25f700
 
-function blurIN(){
-    
-}
-
-
-
-function blurOUT(){
- 
-}
+var display = document.getElementById("minhaDiv").style.display;
+    document.getElementById("minhaDiv").style.display = 'none';
+        function Mudarestadoes(el) {
+                display = document.getElementById("minhaDiv").style.display;
+                     display = document.getElementById(el).style.display;
+                        if(display == "none"){
+                            document.getElementById(el).style.display = 'block'; blurIN()}
+                        else{
+                            document.getElementById(el).style.display = 'none';blurOUT() }
+        }
 
 
 
