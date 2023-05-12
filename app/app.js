caa981b237f536946cec023c7585c0953b29709a
Gabriel Henrique Dagostim Simões <tomuitoop@gmail.com>
2023-05-11 22:14:49 -0300

    blur update

--- app/app.js
+++ app/app.js
@@ -2,15 +2,27 @@
 
 //#25f700
 
+
 var display = document.getElementById("minhaDiv").style.display;
     document.getElementById("minhaDiv").style.display = 'none';
         function Mudarestadoes(el) {
+
+            let eleme = document.getElementById('trava-01');
+
+
+            
+
+
+
                 display = document.getElementById("minhaDiv").style.display;
                      display = document.getElementById(el).style.display;
                         if(display == "none"){
-                            document.getElementById(el).style.display = 'block'; blurIN()}
+                            document.getElementById(el).style.display = 'block';
+                            eleme.classList.add('trava');
+                        }
                         else{
-                            document.getElementById(el).style.display = 'none';blurOUT() }
+                            document.getElementById(el).style.display = 'none';
+                            eleme.classList.remove('trava');}
         }
 
 
